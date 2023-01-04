88ef820acec66ea1c02eeda269d3123e997e9a0b
FauzanRahmat <113502282+FauzanRahmat@users.noreply.github.com>
2023-01-04 18:35:35 +0700

    Add files via upload

---
+++ src/test/java/com/fauzan/anggota/AnggotaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fauzan.anggota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnggotaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
